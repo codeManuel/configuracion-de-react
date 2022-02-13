0866ed8f2d653f7bebe3b26bfefcd569b460a3f0
Manuel <xmanuel.mirandax@gmail.com>
2022-02-12 19:41:26 -0500

    areglando la conexion de vscode con git hup

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import reactDom from "react-dom";
 import App from './components/App';
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        __filename: 'bundle.js'
+        filename: 'bundle.js'
     },
     mode: 'development',
     resolve: {
@@ -29,7 +29,7 @@ module.exports = {
             }
         ]
     },
-    Plugins: [
+    plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html'
