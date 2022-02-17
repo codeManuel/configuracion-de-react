735e7a52574bead9ce734f72844863e521d15394
codeManuel <87095963+codeManuel@users.noreply.github.com>
2022-02-16 21:50:43 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
